7da2344071285bd2fd12d7725a0de6db4fdbfd62
QinJunzhen <1253409131@qq.com>
2016-01-04 11:23:43 +0800

    Happy

---
+++ The weekend to play/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  The weekend to play
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 秦俊珍. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
