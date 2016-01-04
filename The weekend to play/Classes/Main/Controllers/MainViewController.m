64d0cc422085030b293d50882fc9c3e358616890
QinJunzhen <1253409131@qq.com>
2016-01-04 15:07:02 +0800

    The

---
+++ The weekend to play/Classes/Main/Controllers/MainViewController.m
@@ -0,0 +1,44 @@
+//
+//  MainViewController.m
+//  The weekend to play
+//
+//  Created by scjy on 16/1/4.
+//  Copyright © 2016年 秦俊珍. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor cyanColor];
+    
+    
+    
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
